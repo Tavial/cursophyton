efefb607c22e680905d8a2a2fc72c711e92a815d
Tavial <59970729+Tavial@users.noreply.github.com>
2020-04-12 14:40:35 +0200

    Update clases.py

--- modelismoFerroviario/modelo/clases.py
+++ modelismoFerroviario/modelo/clases.py
@@ -3,13 +3,13 @@ Definimos las clases de nuestra aplicación MODELISMO FERROVIARIO
 
 '''
 class Producto ():
-    def __init__(self):
-        self.referencia = ""
-        self.nombre = ""
-        self.tipo = ""
-        self.escala = ""
-        self.fabricante = ""
-        self.precio = 0.0
-        self.stock = 0
-        self.descripcion = ""
-        self.imagen = ""
+    def __init__(self,referencia = "",nombre = "", tipo="Máquinas",escala = "H0",fabricante = "Marklin",precio = 0.0, stock = 0, descripcion="", imagen=""):
+        self.referencia = referencia
+        self.nombre = nombre
+        self.tipo = tipo
+        self.escala = escala
+        self.fabricante = fabricante
+        self.precio = precio
+        self.stock = stock
+        self.descripcion = descripcion
+        self.imagen = imagen
