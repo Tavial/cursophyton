c6ac9927c7f2a99cc70c316f192c72802fee56ac
Tavial <59970729+Tavial@users.noreply.github.com>
2020-04-12 14:42:45 +0200

    Update operaciones_bd.py

--- modelismoFerroviario/modelo/operaciones_bd.py
+++ modelismoFerroviario/modelo/operaciones_bd.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from modelo import constantes_sql
+from modelo import constantes_sql, clases
 
 
 
@@ -37,5 +37,35 @@ def listar_productos():
     #desconectamos la base de datos.
     mydb.disconnect()
     return resultado_lista
+
+def borrar_producto(referencia):
+    sql = constantes_sql.SQL_BORRRAR_PRODUCTO
+    mydb = conectar ()
+    cursor = mydb.cursor()
+    tupla = (referencia,) #ponemos la coma porque si no no lo considera tupla.
+    cursor.execute (sql,tupla)  
+    mydb.commit()
+    mydb.disconnect() 
     
+def obtener_producto(referencia):   
+    sql = constantes_sql.SQL_OBTENER_PRODUCTO_POR_REF
+    mydb = conectar ()
+    cursor = mydb.cursor()
+    tupla = (referencia,)
+    cursor.execute (sql,tupla)
+    producto_obtenido = cursor.fetchone()
+    mydb.disconnect()
+    elemento = clases.Producto(referencia = producto_obtenido[0], nombre = producto_obtenido[1], \
+        tipo = producto_obtenido[2], escala = producto_obtenido[3], fabricante = producto_obtenido[4], \
+        precio = producto_obtenido[5], stock = producto_obtenido[6], descripcion = producto_obtenido[7], \
+        imagen = producto_obtenido[8])
+    return elemento
     
+def guardar_cambios_producto (producto): 
+    sql = constantes_sql.SQL_GUARDAR_CAMBIOS_PRODUCTO
+    mydb = conectar ()
+    cursor = mydb.cursor()
+    tupla = (producto.nombre, producto.tipo, producto.escala, producto.fabricante, producto.precio, producto.stock, producto.descripcion,producto.imagen, producto.referencia)
+    cursor.execute (sql,tupla)
+    mydb.commit()
+    mydb.disconnect()
