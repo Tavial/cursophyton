4415c70e897fe47ae25f9b140358e60b3b0b28f6
Tavial <59970729+Tavial@users.noreply.github.com>
2020-04-15 11:16:45 +0200

    Delete constantes_sql.py

--- modelismoFerroviario/modelo/constantes_sql.py
+++
@@ -1,9 +0,0 @@
-SQL_INSERCION_PRODUCTO ="INSERT INTO `tabla_productos` (`referencia`, `nombre`, `tipo`, `escala`, `fabricante`, `precio`, `stock`, `descripcion`, `imagen`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"
-
-SQL_LISTADO_PRODUCTOS = "SELECT * FROM tabla_productos ;"
-
-SQL_BORRRAR_PRODUCTO = "DELETE FROM tabla_productos WHERE referencia = %s ;"
-
-SQL_OBTENER_PRODUCTO_POR_REF = "SELECT * FROM tabla_productos WHERE referencia = %s ;"
-
-SQL_GUARDAR_CAMBIOS_PRODUCTO = "UPDATE tabla_productos SET nombre = %s, tipo = %s, escala = %s, fabricante = %s, precio = %s, stock = %s, descripcion = %s, imagen = %s WHERE referencia = %s ;"
