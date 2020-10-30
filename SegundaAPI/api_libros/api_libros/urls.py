72d61ea8f55f59504dcb793a32d6acb1fc37cc81
Tavial <59970729+Tavial@users.noreply.github.com>
2020-10-30 15:17:04 +0100

    API funcional Libros

---
+++ SegundaAPI/api_libros/api_libros/urls.py
@@ -0,0 +1,27 @@
+"""api_libros URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+#importando esto puedo crear con path una ruta para generar el token
+from rest_framework.authtoken import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include ('libros.urls')),
+    # Si le hago una petición a esta vista me va a devolver un token de
+    # autorización para el usuario correspondiente
+    path ('api_token/',views.obtain_auth_token),
+]
